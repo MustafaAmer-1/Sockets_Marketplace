d2ad2ac68035fe4353202f9fe55fa42ae0763d63
= <abdullahosama15@gmail.com>
2022-06-28 09:59:26 +0200

    Handling quantity of item

--- Client/UI/scripts/home.js
+++ Client/UI/scripts/home.js
@@ -33,10 +33,10 @@ function showItems(items) {
                 <img src="${item.image}">
                 <h1>${item.name}</h1>
                 <p class="price">$${item.price}</p>
-                <div class="qty" data-max=${item.quantity}>
-                    <span class="minus">-</span>
+                <div class="qty" data-maxqty=${item.quantity}>
+                    <span class="minus" onclick="decrementQty(this)">-</span>
                     <span class="num">0</span>
-                    <span class="plus">+</span>
+                    <span class="plus" onclick="incrementQty(this)">+</span>
                 </div>
                 <p class="cartbtn"><a href="items.html">
                     <button onclick="onclick="addCartEvent(this)">Add to cart</button>
@@ -51,6 +51,30 @@ function showItems(items) {
 
 }
 
+function findSiblingWithClassName(element, className) {
+    let par = element.parentElement
+
+    for(node of par.children) {
+        if(node.classList.contains(className)) return node;
+    } 
+}
+
+function incrementQty(e) {
+    let sibling = findSiblingWithClassName(e, 'num')
+    let qty = parseInt(sibling.innerHTML)
+    let maxQty = parseInt(e.parentElement.dataset.maxqty)
+    if(qty < maxQty)
+        sibling.innerHTML = qty + 1
+}
+
+function decrementQty(e) {
+    console.log(e)
+    let sibling = findSiblingWithClassName(e, 'num')
+    let qty = parseInt(sibling.innerHTML)
+    if(qty > 0)
+        sibling.innerHTML = qty - 1
+        
+}
 
 function HandleTagClick(e) {
     let element = e.target;
