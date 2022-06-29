7ad15e1c33d9c6dc5e561837f1a74c030bab57f8
Abdullah0sama <abdullahosama15@gmail.com>
2022-06-30 00:13:19 +0200

    Allowing nodeIntegration

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ const createWindow = () => {
     const win = new BrowserWindow({
         width: 700,
         height: 600,
+        webPreferences: {
+            nodeIntegration:  true,
+            contextIsolation: false
+        }
     })
     win.removeMenu()
     win.loadFile('./Client/UI/Templates/home.html')
