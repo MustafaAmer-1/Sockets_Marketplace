ddaf3701b48e0f6c6579016f220e2c4b053b5629
Abdullah0sama <abdullahosama15@gmail.com>
2022-06-29 17:44:23 +0200

    Handling deposit and withdraw operations

--- Client/UI/scripts/manage-credit.js
+++ Client/UI/scripts/manage-credit.js
@@ -1,22 +1,42 @@
 
 let userHandler;
-
+let balanceField;
+function responseCallBackGenerator(action) {
+    return function (reqStatus) {
+            if(reqStatus.status) {
+            alert(`successful ${action} operation`)
+        } else {
+            alert("Could not perform " + action)
+        }
+    }
+}
 function populateBalance(userData) {
-    console.log(userData)
-    document.getElementsByName('balance')[0].value = userData.balance
+    balanceField.value = userData.balance
+}
+
+function depositEvent(amount) {
+    userHandler.deposit(amount, responseCallBackGenerator('Deposit'))
+}
+function withdrawEvent(amount) {
+    userHandler.withdraw(amount, responseCallBackGenerator('Withdraw'))
+}
+
+
+function getConfirmationfordeposit() {
+    var resp = parseInt(window.prompt("Please enter Deposit Amount"))
+    depositEvent(resp)
+}
+
+function getConfirmationforredraw() {
+    var resp = parseInt(window.prompt("Please enter withdrawal Amount"))
+    if(resp > balanceField.value) alert("No sufficient  money to fulfill your operation.")
+    else withdrawEvent(resp)
+    
 }
 
 function startUp() {
     userHandler = new UserHandler()
+    balanceField = document.getElementsByName('balance')[0]
     userHandler.getUserInfo(populateBalance)
 }
-
-function getConfirmationfordeposit() {
-    var resp = window.prompt("Please enter Deposit Amount")
-}
-
-function getConfirmationforredraw() {
-    var resp = window.prompt("Please enter withdrawal Amount")
-}
-
 startUp()
