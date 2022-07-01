0f2f94845981414fadf29d13f23c415d280e2383
Mustafa Amer <Eng.MustafaAmer0@gmail.com>
2022-07-01 07:11:50 +0200

    integrate data

--- main.js
+++ main.js
@@ -4,15 +4,14 @@ const onExit = require('./Client/DataHandler/ServerConnection').onExit
 
 const createWindow = () => {
     const win = new BrowserWindow({
-        width: 700,
-        height: 600,
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false
         },
         icon: './Client/UI/Templates/trolilogo.jpg'
     })
-    win.removeMenu()
+
+    win.maximize()
     win.loadFile('./Client/UI/Templates/login.html')
     win.on('close', () => {
         onExit()
