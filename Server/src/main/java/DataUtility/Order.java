59ac0abd1ae455743fef65b06ea206bd8211bc26
mizo7757 <moazayman@hotmail.com>
2022-07-02 01:52:59 +0200

    Get Customer History function
    Co-Authored-By: ayatarek831 <64319299+ayatarek831@users.noreply.github.com>

--- Server/src/main/java/DataUtility/Order.java
+++ Server/src/main/java/DataUtility/Order.java
@@ -1,15 +1,37 @@
 package DataUtility;
 
+import java.util.ArrayList;
 import java.util.Date;
 
-public class Order extends Container {
-    private final Date dateCreated;
+public class Order<Arraylist> {
+    //private final Date dateCreated;
+    private final int OID;
+    private final int CustID;
+    private String name;
+    private int quantity;
+    private final float total_price;
+    private ArrayList<Item> items = new ArrayList();
+    public Order(int oid,int custid,float price){
 
-    public Order(Date dateCreated){
-        this.dateCreated = dateCreated;
+        this.OID = oid;
+        this.CustID=custid;
+        this.total_price=price;
+    }
+    public int getOID() {
+        return OID;
+    }
+    public void additem(int PID,String name, float price, int quantity, String imgURL, String categorie)
+    {
+        items.add(new Item( PID,name,price, quantity,  imgURL, categorie));
+        this.quantity=quantity;
+        this.name = name;
+    }
+    public ArrayList<Item> getItems() {
+        return items;
     }
 
-    public Date getDateCreated() {
-        return dateCreated;
+    public float getTotal_price() {
+        return total_price;
     }
+
 }
